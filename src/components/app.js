b7d986bb9bd94ab574794340769663a18d1e7205
Daniel <dnlsndbrg@gmail.com>
2017-06-22 13:47:35 +0200

    trying socket.io connection

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,13 @@ import {h} from 'preact';
 import {Provider} from 'preact-redux';
 import Title from './title';
 
+import io from 'socket.io-client';
+const socket = io('localhost:3000');
+
+socket.on('connect', () => {
+  console.log('connected');
+});
+
 const App = ({store}) => {
   return (
     <Provider store={store}>
